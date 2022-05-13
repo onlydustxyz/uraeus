3d72b07c7bedd8f736f598c2efe41f1ad3cc6594
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2022-05-13 11:53:46 +0200

    🥅 use anyhow for errors

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,13 @@
 pub mod term;
 pub mod verify;
-
+use anyhow::Result;
 use clap::Command;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
 const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
-pub fn execute() -> Result<(), &'static str> {
+pub fn execute() -> Result<()> {
     let matches = Command::new("uraeus")
         .version(VERSION)
         .author(AUTHORS)
--- src/compiler/protostar.rs
+++ src/compiler/protostar.rs
@@ -1,8 +1,9 @@
+use anyhow::{anyhow, Result};
 use execute::Execute;
 use log::debug;
 use std::process::Command;
 
-pub fn compile(project_dir: String, build_dir: String) -> Result<(), &'static str> {
+pub fn compile(project_dir: String, build_dir: String) -> Result<()> {
     debug!(
         "Entering compile with args\nproject_dir: {}\nbuild_dir: {}",
         project_dir, build_dir
@@ -14,10 +15,10 @@ pub fn compile(project_dir: String, build_dir: String) -> Result<(), &'static st
     let output = compile_command.execute_output().unwrap();
     if let Some(exit_code) = output.status.code() {
         if exit_code != 0 {
-            return Err("Build command failed");
+            return Err(anyhow!("Build command failed"));
         }
     } else {
-        return Err("Build command interrupted!");
+        return Err(anyhow!("Build command interrupted!"));
     }
     println!("Compilation completed.");
     Ok(())
