e52f03965f46739cc4db9f4668967cba7752e3ac
gregory <10611760+gregoryguillou@users.noreply.github.com>
2022-07-02 16:12:05 +0200

    add css and font. not yet slick

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import { sources, refresh as sourceRefresh } from '$lib/stores/sources';
+	import '../app.css';
 
 	let selectedSource = '';
 
@@ -46,30 +47,35 @@
 	};
 </script>
 
-<h2>Verify</h2>
-<p>For now, type 0x0000000000000000000000000000000000000001</p>
-<select bind:value={selectedSource}>
-	{#each $sources.sources as source}
-		<option value={source}>
-			{source}
-		</option>
-	{/each}
-</select>
+{#if $sources.lastError}
+	{$sources.lastError}
+{:else}
+	<h2>Verify</h2>
+	<p>For now, type 0x0000000000000000000000000000000000000001</p>
+	<select bind:value={selectedSource}>
+		{#each $sources.sources as source}
+			<option value={source}>
+				{source}
+			</option>
+		{/each}
+	</select>
 
-<input type="text" bind:value={address} />
-<input type="button" value="reset" on:click={reset} />
-<input type="button" value="check" on:click={check} />
-<p>
-	{#if !$contract.init}
-		{#if $contract.loading}
-			Loading...
-		{:else if $contract.lastError}
-			{$contract.lastError}
-		{:else if $contract.success}
-			✅ {$contract.address} matches {$contract.source}
-		{:else}
-			❌ {$contract.address} does not match {$contract.source}
+	<input type="text" bind:value={address} />
+	<input type="button" value="reset" on:click={reset} />
+	<input type="button" value="check" on:click={check} />
+	<p>
+		{#if !$contract.init}
+			{#if $contract.loading}
+				Loading...
+			{:else if $contract.lastError}
+				{$contract.lastError}
+			{:else if $contract.success}
+				✅ {$contract.address} matches {$contract.source}
+			{:else}
+				❌ {$contract.address} does not match {$contract.source}
+			{/if}
 		{/if}
-	{/if}
-</p>
+	</p>
+{/if}
+
 <footer>built with ❤ by <a href="https://onlydust.xyz">onlydust.xyz</a></footer>
